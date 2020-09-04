600f31744ff442e5db02c56a640cf0b87631841f
Lior Banai <liorbanai@gmail.com>
2020-09-04 15:23:55 +0300

    add UI elements

---
+++ Analogy.CommonUtilities/UI/AnalogyPropertiesMatcherUC.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using Analogy.Interfaces;
+
+namespace Analogy.CommonUtilities.UI
+{
+    public partial class AnalogyPropertiesMatcherUC : UserControl
+    {
+        private AnalogyLogMessagePropertyName Selection { get; set; }
+        private ILogParserSettings ParserSettings { get; set; }
+        public AnalogyPropertiesMatcherUC()
+        {
+            InitializeComponent();
+        }
+
+        public AnalogyPropertiesMatcherUC(ILogParserSettings parserSettings) : this()
+        {
+            ParserSettings = parserSettings;
+        }
+        private void AnalogyPropertiesMatcherUC_Load(object sender, EventArgs e)
+        {
+            cbLogProperties.DataSource = AnalogyLogMessage.AnalogyLogMessagePropertyNames.Values.ToList();
+            cbLogProperties.DropDownStyle = ComboBoxStyle.DropDownList;
+            Selection = (AnalogyLogMessagePropertyName)cbLogProperties.SelectedItem;
+            UpdateMappings();
+            cbLogProperties.SelectedIndexChanged += this.cbLogProperties_SelectedIndexChanged;
+
+        }
+
+        private void cbLogProperties_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Selection = (AnalogyLogMessagePropertyName)cbLogProperties.SelectedItem;
+            lblInfo.Text = $"Log files keys/properties to map to {Selection}:";
+            UpdateMappings();
+
+        }
+
+        private void UpdateMappings()
+        {
+            if (ParserSettings.Maps.ContainsKey(Selection))
+            {
+                lstbMappedKeys.Items.Clear();
+                lstbMappedKeys.Items.AddRange(ParserSettings.Maps[Selection].ToArray());
+            }
+        }
+
+        private void btnAddKey_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtKey.Text))
+            {
+                ParserSettings.AddMap(Selection, txtKey.Text);
+                UpdateMappings();
+            }
+        }
+
+        private void lstbMappedKeys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblSelection.Text = "Selection:" + lstbMappedKeys.SelectedItem;
+        }
+
+        private void btnDeleteSelection_Click(object sender, EventArgs e)
+        {
+            if (lstbMappedKeys.SelectedItem != null)
+            {
+                ParserSettings.DeleteMap(Selection, lstbMappedKeys.SelectedItem as string);
+                UpdateMappings();
+            }
+        }
+    }
+}
