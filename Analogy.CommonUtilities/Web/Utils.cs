98eed1227224249fa0815b29bbd0b3fd3964fe04
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-24 16:00:10 +0300

    add web tool get

---
+++ Analogy.CommonUtilities/Web/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Analogy.CommonUtilities.Web
+{
+   public static class Utils
+    {
+        public static async Task<(bool newData, T result)> GetAsync<T>(string uri,string userAgent, string token, DateTime lastModified)
+        {
+            try
+            {
+                Uri myUri = new Uri(uri);
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(myUri);
+                myHttpWebRequest.Accept = "application/json";
+                myHttpWebRequest.UserAgent = userAgent;
+                if (!string.IsNullOrEmpty(token))
+                    myHttpWebRequest.Headers.Add(HttpRequestHeader.Authorization, $"Token {token}");
+
+                myHttpWebRequest.IfModifiedSince = lastModified;
+
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)await myHttpWebRequest.GetResponseAsync();
+                if (myHttpWebResponse.StatusCode == HttpStatusCode.NotModified)
+                    return (false, default)!;
+
+                using (var reader = new System.IO.StreamReader(myHttpWebResponse.GetResponseStream()))
+                {
+                    string responseText = await reader.ReadToEndAsync();
+                    return (true, JsonConvert.DeserializeObject<T>(responseText));
+                }
+            }
+            catch (WebException e) when (((HttpWebResponse)e.Response).StatusCode == HttpStatusCode.NotModified)
+            {
+                return (false, default)!;
+            }
+            catch (Exception)
+            {
+                return (false, default)!;
+            }
+        }
+
+    }
+}
